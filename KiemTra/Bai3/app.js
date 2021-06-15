9adf59976bafef97557be853339d6cd95b342014
Bui Xuan Hieu <buixuanhieutn@gmail.com>
2021-06-15 20:47:53 +0700

    dang bai

---
+++ KiemTra/Bai3/app.js
@@ -0,0 +1,13 @@
+let input = document.getElementById("input").value;
+let ketqua = 23;
+// input.addEventListener("submit", function(e) {
+//     if (input.dt.value.trim() === "") {
+//         return;
+//     }
+// })
+
+if (input == ketqua) {
+    alert("Bạn đã đoán đúng")
+} else {
+    alert("đoán lại")
+}
